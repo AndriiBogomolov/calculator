2d13754a7516a906632383d2a416ae7bdf75c62f
Andrii Bogomolov <bogomolov.andrii@gmail.com>
2025-04-30 00:07:21 +0200

    update calculator buttons

--- src/app/components/buttons/CalculatorButton.jsx
+++ src/app/components/buttons/CalculatorButton.jsx
@@ -1,12 +1,28 @@
 "use client";
-const CalculatorButton = ({ label, onClick, className = "" }) => (
-  <button
-    onClick={onClick}
-    className={`w-[71px] h-[72px] rounded-[24px] bg-[var(--dark-button-low-emphasis)] text-[32px] leading-[125%] font-normal text-[var(--dark---text)] text-center`}
-  >
-    {label}
-  </button>
-);
+
+const CalculatorButton = ({ label, onClick, type = "number" }) => {
+  const baseStyles = "w-[71px] h-[72px] rounded-[24px] text-[32px] leading-[125%] font-normal text-center";
+  const textColor = "text-[var(--dark---text)]";
+
+  let bgColor;
+  switch (type) {
+    case "operator":
+      bgColor = "bg-[var(--dark-button-high-emphasis)]";
+      break;
+    case "function":
+      bgColor = "bg-[var(--dark-button-medium-emphasis)]";
+      break;
+    default:
+      bgColor = "bg-[var(--dark-button-low-emphasis)]";
+  }
+
+  return (
+    <button onClick={onClick} className={`${baseStyles} ${bgColor} ${textColor}`}>
+      {label}
+    </button>
+  );
+};
 
 export default CalculatorButton;
+
 
